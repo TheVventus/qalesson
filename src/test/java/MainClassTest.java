8204d948caa5150090e145e8c773dbe03e6c4eee
Ildar <thevventus@gmail.com>
2022-06-19 14:57:30 +0400

    Feature: complete test 2

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -7,4 +7,8 @@ public class MainClassTest extends MainClass {
     public void testGetLocalNumber() {
         Assert.assertTrue("Тест не равен 14",this.getLocalNumber() == 14);
     }
+
+    @Test public void testGetClassNumber() {
+        Assert.assertTrue("Тестовое значение равно не больше 45",this.getClassNumber() > 45);
+    }
 }
